f69f189d829cec820bd53fa89b00ddcfea363915
Oleksii Tsvietaiev <o.tsvietaiev92@gmail.com>
2024-10-21 21:40:15 +0300

    fix(test): exclude firebase & pub/sub from test environment

--- src/main/java/org/devcalm/stocks/configuration/GooglePubSubConfiguration.java
+++ src/main/java/org/devcalm/stocks/configuration/GooglePubSubConfiguration.java
@@ -8,11 +8,13 @@ import org.devcalm.stocks.configuration.properties.GoogleCloudProviderProperties
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.messaging.MessageChannel;
 
 @Slf4j
 @Configuration
+@Profile("!test")
 public class GooglePubSubConfiguration {
 
     @Bean
